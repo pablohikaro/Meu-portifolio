b04951a74433cff8c2b89e643026ae56b518b105
Pablohikaro <pablozeus50@gmail.com>
2025-04-07 22:22:10 -0300

    cria menu mobile e add logo

---
+++ SRC/js/menu.js
@@ -0,0 +1,54 @@
+const btnMenu = document.getElementById("hamburger-button")
+const menu = document.getElementById("menu")
+
+btnMenu.classList.add("hamburger-button-js-enabled")
+
+function closeMenu() {
+    btnMenu.setAttribute("aria-expanded", "false")
+    menu.setAttribute("aria-hidden", "true")
+    menu.classList.add("menu-closed")
+}
+
+closeMenu()
+
+
+btnMenu.addEventListener("click", function(){
+    console.log(this)
+
+    let expanded = this.getAttribute("aria-expanded") === "true" ? true : false
+
+    document.removeEventListener("click", closeMenu)
+
+    if(expanded) {
+        menu.classList.add("menu-closed")
+    }else{
+        menu.classList.remove("menu-closed")
+    }
+
+    this.setAttribute("aria-expanded", !expanded)
+    menu.setAttribute("aria-hidden", expanded)
+
+
+     setTimeout(function(){
+        if(!expanded) {
+            document.addEventListener("click", closeMenu)
+        }
+     }, 1)
+})
+
+const mediaQuery = window.matchMedia('(min-width: 768px')
+
+function handleMediaQueryChange(e){
+    if(e.matches){
+        menu.setAttribute("aria-hidden", "false")
+        btnMenu.setAttribute("aria-expanded", "true")
+        menu.classList.remove("menu-closed")
+    }else{
+        menu.setAttribute("aria-hidden", "true")
+        btnMenu.setAttribute("aria-expanded", "false")
+        menu.classList.add("menu-closed")
+    }
+}
+
+midiaQuery.addEventListener("change", handleMediaQueryChange)
+handleMediaQueryChange(midiaQuery)
